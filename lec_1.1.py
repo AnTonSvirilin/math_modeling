0d09927117e73c21dc635fdef8db3c5199eb26fc
AnTonSvirilin <146557772+AnTonSvirilin@users.noreply.github.com>
2023-10-07 16:04:31 +0000

    kkkkk

---
+++ lec_1.1.py
@@ -0,0 +1,9 @@
+a = input('Введите знаение а:')
+print(a)
+print(type(a))
+ 
+
+a = int(input('Введите знаение а:'))    #число 
+print(a)
+print(type(a))
+
---
+++ lec_1.py
@@ -0,0 +1,14 @@
+s = 3 + 4 
+print(s)
+
+type(3)  #опред тип данных 
+print(type(3))
+print(type(3.4))  # float  - число с плав запятой
+
+print(type('molodec'))  # str - stroka 
+a = ['molodec', 1, 6]    # список 
+
+
+print(type(True)) # bool 
+
+a = input()    #вызов числа из консоли 
