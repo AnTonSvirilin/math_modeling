466bf152d68a0089dab598568ed543a01d24ee6a
AnTonSvirilin <146557772+AnTonSvirilin@users.noreply.github.com>
2023-11-06 12:45:08 +0200

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,3 +1,31 @@
+import numpy as np
+from math import sin
 
+N = int(input("Введите значение N: "))
+M = int(input("Введите значение M: "))
+zero_index = input("Используется ли индексация с нуля? ")
+
+if zero_index == 'да':
+    array = np.zeros((N, M))
+    for i in range(N):
+        for j in range(M):
+            a = sin(N*(i+1) + M*(j+1))
+            if a < 0:
+                array[i, j] = 0
+             else:
+                array[i, j] = a
+else:
+    array = np.zeros((N+1, M+1))
+    for i in range(1, N+1):
+        for j in range(1, M+1):
+            a = sin(N*i + M*j)
+            if a < 0:
+                array[i, j] = 0
+            else:
+                array[i, j] = a
 
 array[:, [column1, column2]] = array[:, [column2, column1]]
+
+print(array)
+
+
