4ec67d33207e07f9e4de042786d032714a229862
AnTonSvirilin <146557772+AnTonSvirilin@users.noreply.github.com>
2023-12-02 15:25:17 +0000

    itstiiiime

---
+++ task3.py
@@ -0,0 +1,17 @@
+import time 
+
+M, N = int(input()), int(input())
+
+starttimer = time.time()
+
+
+for i in range(0, N):
+    print(i)
+    for k in range(0,M):
+        print(k)
+        time.sleep(1)
+
+endtimer = time.time()
+
+total = endtimer - starttimer
+print(total)
