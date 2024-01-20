2b824e8a04a2a252cd5e3d6e0296084dd8dc1473
AnTonSvirilin <146557772+AnTonSvirilin@users.noreply.github.com>
2024-01-20 13:15:54 +0000

    сс

--- 123.py
+++ 123.py
@@ -8,7 +8,7 @@ potato_rect = plt.Rectangle((0.4, 0.9), 0.2, 0.05, fc='orange')
 ax.add_patch(potato_rect)
 
 # Создание лужи (голубой овал)
-puddle_oval = plt.Rectangle((0.3, -0.4), 0.4, 0.2, fc='blue')
+puddle_oval = plt.Rectangle((0.3, 0.4), 0.4, 0.2, fc='blue')
 ax.add_patch(puddle_oval)
 
 def update(frame):
@@ -16,9 +16,12 @@ def update(frame):
         # Падение картошки фри в лужу
         potato_rect.set_y(potato_rect.get_y() - 0.02)
     else:
-        # Растекание лужи
-        puddle_oval.set_height(puddle_oval.get_height() - 0.02)
-        puddle_oval.set_y(puddle_oval.get_y() - 0.01)
+        # Растекание лужи вправо и влево
+        if frame % 2 == 0:
+            puddle_oval.set_width(puddle_oval.get_width() + 0.02)
+            puddle_oval.set_x(puddle_oval.get_x() - 0.01)
+        else:
+            puddle_oval.set_width(puddle_oval.get_width() + 0.02)
     
     return potato_rect, puddle_oval
 
@@ -27,4 +30,7 @@ def init():
 
 ani = animation.FuncAnimation(fig, update, frames=100, init_func=init, blit=True)
 
+plt.axis('scaled')
+plt.xlim(0, 1)
+plt.ylim(0, 1)
 plt.show()
