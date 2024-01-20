3d04f1046029955ff88153587afcee194892684b
AnTonSvirilin <146557772+AnTonSvirilin@users.noreply.github.com>
2024-01-20 13:11:41 +0000

    ddd

--- 123.py
+++ 123.py
@@ -3,16 +3,16 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
-# Создание картошки фри (желтый прямоугольник)
-potato_rect = plt.Rectangle((0.5, 0.9), 0.2, 0.1, fc='yellow')
+# Создание картошки фри (оранжевый прямоугольник)
+potato_rect = plt.Rectangle((0.4, 0.9), 0.2, 0.05, fc='orange')
 ax.add_patch(potato_rect)
 
 # Создание лужи (голубой овал)
-puddle_oval = plt.Rectangle((0.4, -0.4), 0.4, 0.2, fc='blue')
+puddle_oval = plt.Rectangle((0.3, -0.4), 0.4, 0.2, fc='blue')
 ax.add_patch(puddle_oval)
 
 def update(frame):
-    if frame < 50:  # 50 кадров, чтобы анимация длилась 5 секунд
+    if frame < 50:
         # Падение картошки фри в лужу
         potato_rect.set_y(potato_rect.get_y() - 0.02)
     else:
